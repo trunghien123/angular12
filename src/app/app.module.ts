804a52455cdff79efd0115eee5540444f5292bfa
trunghien <hienhuynh076@gmail.com>
2021-06-01 14:30:23 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavBarComponent } from './shares/nav-bar/nav-bar.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiServe : "http://api.openweathermap.org"
+};
